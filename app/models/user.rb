4406cdf182b0b50a4637f151a3c6b0bf8d7ebdd8
441daichi <guruguru441@gmail.com>
2024-07-15 20:21:32 +0900

    userのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers'
+
+         validates :nickname, uniqueness: true, presence: true
+         validates :password, presence: true
 end
