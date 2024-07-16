22b7ea9f63a741ae6523cf4d0bb9d32af55c3472
441daichi <guruguru441@gmail.com>
2024-07-16 22:04:04 +0900

    バリデーションの実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,12 @@ class Review < ApplicationRecord
   belongs_to :evaluation
 
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre_id, presence: true, numericality: { other_than: 0 }
+  validates :tag, numericality: { only_integer: true, less_than_or_equal_to: 20 }
+  validates :evaluation_id, presence: true, numericality: { other_than: 0 }
+  validates :impression, numericality: { only_integer: true, less_than_or_equal_to: 300 }
 end
