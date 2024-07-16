0b0b06e4dc30aeebf286aecc499f86d57828bf15
441daichi <guruguru441@gmail.com>
2024-07-16 21:50:32 +0900

    ストロングパラメーターを記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,9 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:image, :title, :author, :genre_id, :tag, :evaluation_id, :impression ).merge(user_id: current_user.id)
+  end
 end
