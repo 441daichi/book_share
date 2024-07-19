a6f10028c4f9f41bc0416f5e62460546cd15d0e0
441daichi <guruguru441@gmail.com>
2024-07-20 08:29:23 +0900

    マイグレーションの実行

--- db/migrate/20240719232625_create_comments.rb
+++ db/migrate/20240719232625_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      t.references :user,              null: false, foreign_key: true
+      t.references :review,            null: false, foreign_key: true
+      t.text :comment_text,            null: false
       t.timestamps
     end
   end
