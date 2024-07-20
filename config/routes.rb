b3468544779305460392314e3752b8f5dc1065c9
441daichi <guruguru441@gmail.com>
2024-07-20 16:18:50 +0900

    updateアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'reviews#index'
 
-  resources :reviews, only: [:index, :new, :create, :show, :edit] do
+  resources :reviews, only: [:index, :new, :create, :show, :edit, :update] do
     resources :comments, only: [:new, :create]
   end
 end
