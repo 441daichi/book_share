6caf90c48aff51b8a9e287aedb73d3e7cf83fed1
441daichi <guruguru441@gmail.com>
2024-07-20 10:03:59 +0900

    コメント投稿ページのレイアウトを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'reviews#index'
+
   resources :reviews, only: [:index, :new, :create, :show] do
-    resources :comments, only: [:new]
+    resources :comments, only: [:new, :create]
   end
 end
