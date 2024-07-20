f850c077506e54e1ee98885df6c43111c6cf81ce
441daichi <guruguru441@gmail.com>
2024-07-20 10:34:17 +0900

    テスト内容を記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'コメントの保存' do
+    context 'コメントの保存ができるとき' do
+      it 'コメントのテキストが存在すれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントの保存ができないとき' do
+      it 'コメントのテキストが空だと保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'コメントのテキストが100字を超えると保存できない' do
+        @comment.text =  'a' * 101
+        @comment.valid?
+        expect(comment.errors.full_messages).to include("Text is too long (maximum is 100 characters)")
+      end
+    end
+  end
 end
